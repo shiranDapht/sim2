104b0c6794f40f1fa13fed6c5574015a10040d00
Shiran <93221484+shiranDapht@users.noreply.github.com>
2022-01-15 23:00:31 +0200

    Add files via upload

---
+++ mult16x16.s
@@ -0,0 +1,43 @@
+# Operands to multiply
+.data
+a: .word 0xBAD
+b: .word 0xFEED
+
+.text
+main:   # Load data from memory
+		la      t3, a
+        lw      t3, 0(t3)
+        la      t4, b
+        lw      t4, 0(t4)
+        
+        # t6 will contain the result
+        add		t6, x0, x0
+
+        # Mask for 16x8=24 multiply
+        ori		t0, x0, 0xff
+        slli	t0, t0, 8
+        ori		t0, t0, 0xff
+        slli	t0, t0, 8
+        ori		t0, t0, 0xff
+        
+####################
+# Start of your code
+
+		andi t1,t3,0xff 
+        mul t6, t4, t1 
+        and t6, t6, t0  
+        srli t1, t3, 8 
+        mul t1, t4, t1 
+        and t1, t1, t0 
+        slli t1,t1, 8
+        add t6, t6, t1 
+# End of your code
+####################
+		
+finish: addi    a0, x0, 1
+        addi    a1, t6, 0
+        ecall # print integer ecall
+        addi    a0, x0, 10
+        ecall # terminate ecall
+
+
